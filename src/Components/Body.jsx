bcc34afd7a7fd86c0553b17ebfc7f269be1c8b72
Darshil Thummar <darshil.thummar2020@vitbhopal.ac.in>
2024-03-27 19:09:09 +0530

    1st commit

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Body = () => {
   return (
-    <div>Body</div>
+    <div>
+      <div>
+
+      </div>
+      <div>
+        <img src="./public/assets/Blue-shape.svg" alt="1st" />
+        <img src="./public/assets/Purple-shape.svg" alt="2nd" />
+        <img src="./public/assets/Pink-shape.svg" alt="3rd" />
+        <img src="./public/assets/Hero-model.png" alt="model" />
+      </div>
+    </div>
   )
 }
 
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { FaBars } from 'react-icons/fa'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='flex justify-between'>
+      <div className='flex justify-center gap-2'>
+        <img src="./public/assets/Logo.svg" alt="logo" />
+        <button className='bg-gradient-to-r from-red-500 to-orange-500 text-white font-bol rounded-full text-sm px-2 py-1'>We are Hiring</button>
+      </div>
+      <div>
+        <FaBars />
+      </div>
+    </div>
   )
 }
 
